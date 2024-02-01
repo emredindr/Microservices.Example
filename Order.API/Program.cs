cf831cfe5bc0c520c57f5fb361fc6ca0fa97bd14
Emre DİNDAR <75311655+emredindr@users.noreply.github.com>
2024-02-01 21:27:22 +0300

    Add project files.

---
+++ Order.API/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<OrderAPIDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer")));
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
